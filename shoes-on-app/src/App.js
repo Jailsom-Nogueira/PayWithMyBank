ab40f3aa01d1221f54385dfda036878c69ce4aab
Jailsom Nogueira <jailsom.nogueira@gmail.com>
2020-10-30 17:39:44 -0300

    rendering products

--- shoes-on-app/src/App.js
+++ shoes-on-app/src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import Router from './Router';
+import styled from 'styled-components';
+
+import { GlobalStorage } from './GlobalContext';
+
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: lightgray;
+  height: 100vh;
+`;
+
+const AppWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-height: 80vh;
+  background-color: white;
+  padding: 0 8px;
+`;
 
 function App() {
   return (
     <div>
-      <Router />
+      <GlobalStorage>
+        <AppContainer>
+          <AppWrapper>
+            <Router />
+          </AppWrapper>
+        </AppContainer>
+      </GlobalStorage>
     </div>
   );
 }
