b37060bce56b6f4a8cd4adba8272b64b4a6e97ae
Jailsom Nogueira <jailsom.nogueira@gmail.com>
2020-11-02 14:53:52 -0300

    Added 'Back' button

--- shoes-on-app/src/components/NavBar/index.js
+++ shoes-on-app/src/components/NavBar/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import UserAvatar from './UserAvatar';
 import styled from 'styled-components';
+
+import UserAvatar from './UserAvatar';
+
+import GoBackButton from '../../components/Buttons/GoBackButton';
 
 const NavBarContainer = styled.div`
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
+  align-items: center;
   width: 100%;
   padding: 0.55rem;
+  margin-bottom: 0.75rem;
 `;
 
 export default function NavBar() {
   return (
     <NavBarContainer>
+      <GoBackButton />
+
       <UserAvatar />
     </NavBarContainer>
   );
